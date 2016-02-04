a724047eca4db49f321ab30d715062b816cbc650
Douglas Lira <douglas.lira.web@gmail.com>
2016-02-04 08:22:53 -0200

    Create structure

---
+++ tests/karma.conf.js
@@ -0,0 +1,36 @@
+module.exports = function(config){
+    config.set({
+
+        basePath : '../',
+
+        files : [
+            'bower_components/angular/angular.js',
+            'bower_components/angular-route/angular-route.js',
+            'bower_components/angular-mocks/angular-mocks.js',
+            'app/**/*/*.js',
+            'tests/**/*/*.js'
+        ],
+
+        autoWatch : true,
+
+        frameworks: ['jasmine'],
+
+        browsers : ['PhantomJS'],
+
+        plugins : [
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-jasmine',
+            'karma-junit-reporter',
+            'karma-phantomjs-launcher'
+        ],
+
+        junitReporter : {
+            outputFile: 'test_out/unit.xml',
+            suite: 'unit'
+        },
+        singleRun: false,
+        colors: true,
+        logLevel: config.LOG_DEBUG
+    });
+};
