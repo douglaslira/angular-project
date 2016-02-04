3915dad42d64003bec8955d573450aa901f3f625
Douglas Lira <douglas.lira.web@gmail.com>
2016-02-04 21:53:36 -0200

    Travis build e2e

--- tests/protractor.conf.js
+++ tests/protractor.conf.js
@@ -1,7 +1,7 @@
 var jasmineReporters = require('jasmine-reporters');
 exports.config = {
-  baseUrl: 'http://localhost:8000/app/',
-  seleniumServerJar: '../node_modules/protractor/selenium/selenium-server-standalone-2.47.1.jar',
+  baseUrl: 'http://localhost:9876/app/',
+  seleniumServerJar: 'node_modules/protractor/selenium/selenium-server-standalone-2.47.1.jar',
   seleniumPort: 4444,
   seleniumArgs: ['-browserTimeout=60'],
   framework: 'jasmine',
